accc21d5552db92914b47d4ca9ef67e9f8ec32d4
MrLost <MrLost@DESKTOP-A64DPCU>
2026-09-20 22:04:34 +0330

    Add Framework folder and base classes to AppCore
    Added Framework folder reference to AppCore.Auth, BLL, and DAL .csproj files. Introduced AppCore.Model.Framework namespace with ApplicationServiceResponse and ApplicationServiceResponse<T> for service operation results. Added BaseEntity class with long Id property for entity inheritance.

---
+++ AppCore.Model/Framework/ApplicationServiceResponse.cs
@@ -0,0 +1,41 @@
+﻿namespace AppCore.Model.Framework;
+
+public class ApplicationServiceResponse
+{
+    private List<string> _errors = new List<string>();
+    private List<string> _messages = new List<string>();
+
+
+    public void AddError(string error)
+    {
+        _errors.Add(error);
+    }
+
+    public void AddError(List<string> errors)
+    {
+        _errors.AddRange(errors);
+    }
+
+    public void AddMessage(string message)
+    {
+        _messages.Add(message);
+    }
+
+    public void AddMessage(List<string> messages)
+    {
+        _messages.AddRange(messages);
+    }
+
+    public List<string> Errors => _errors;
+    public List<string> Messages => _messages;
+
+    public bool IsSuccess => !IsFailure;
+    public bool IsFailure => _errors.Any();
+}
+
+
+public class ApplicationServiceResponse<T> : ApplicationServiceResponse
+    where T : class
+{
+    public T Result { get; set; }
+}
---
+++ AppCore.Model/Framework/BaseEntity.cs
@@ -0,0 +1,12 @@
+﻿namespace AppCore.Model.Framework;
+
+/// <summary>
+/// کلاس پایه موجودیت ها
+/// </summary>
+public class BaseEntity
+{
+    /// <summary>
+    /// کلید اصلی موجودیت
+    /// </summary>
+    public long Id { get; set; }
+}
